8fcb175dc7b830c83acf132a453a1741c2c8ab3a
inevadim <78503414+inevadim@users.noreply.github.com>
2021-11-06 20:31:59 +0300

    add folder components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { leftBar } from './components/leftBar/leftBar';
 
 function App() {
   return (
     <div className="App">
-   
+      <leftBar />
     </div>
   );
 }
---
+++ src/components/leftBar/leftBar.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const leftBar = () => {
+  return <div>leftBar</div>;
+};
