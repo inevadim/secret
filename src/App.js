d44c6fcd493a66a13568f1174e4804a35a047461
vadim <vadim@192.168.1.93>
2021-11-06 22:28:35 +0300

    add structure header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Header } from './components/header/Header';
 
 function App() {
   return (
-      
       <div className={styles.wrapper}>
         <Header />
         {/* <LeftBar /> */}
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,20 @@
 import styles from "./Header.module.scss"
+import imgSettings from "./../../assets/img/setting.svg"
 
 export const Header = () =>{
     return(
-        <div className={styles.header}>
-            <div className={styles.logo}>IneVadim</div>
-            <div className={styles.settings}>Settings</div>
+        <div className={styles.wrapper}>
+            <div className={styles.header}>
+                <div className={styles.logo}>IneVadim</div>
+                <div className={styles.settings}>
+                    <img src={imgSettings} alt="settings"/>
+                </div>
+            </div>
+            <div className={styles.toogleHeaderWrapper}>
+                <div className={styles.toogleHeader}>
+                    toogleHeader
+                </div>
+            </div>
         </div>
     )
 }
